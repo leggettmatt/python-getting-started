74ccd39ccaa3a0ba01ed26615f7ec828b73112f2
Matt Leggett <mleggett@counsyl.com>
2014-11-20 12:32:48 -0800

    v 2.0 hello

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    return HttpResponse('Hello from Python v2.0!')
 
 
 def db(request):
